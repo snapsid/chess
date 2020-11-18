8b2a09e1a147cd562225909cea170f5142acadc1
snapsid <siddharthchopra555@gmail.com>
2020-11-19 00:11:35 +0530

    attack moves for pawn

--- attack.c
+++ attack.c
@@ -27,34 +27,7 @@ int SqAttacked(const int sq, const int side, const Board_struct *pos) {
 
 
 
-  // for knights
-	for(index = 0; index < 8; ++index) {
-		pce = pos->pices[sq + KnDir[index]];
-		if(IsKn(pce) && PieceColour[pce]==side) {
-			return TRUE;
-		}
-	}
-  // it will check if from that square - 8 is there any knight and also if its color is same as the move color
-  // it wil than return true
 
-
-
-  // rooks, queens
-  for(index = 0; index < 4; ++index) {
-    dir = RkDir[index];
-    t_sq = sq + dir;
-    pce = pos->pices[t_sq];
-    while(pce != OFFBOARD) {
-      if(pce != EMPTY) {
-        if(IsRQ(pce) && PieceColour[pce] == side) {
-          return TRUE;
-        }
-        break;
-      }
-      t_sq += dir;
-      pce = pos->pices[t_sq];
-    }
-  }
 
 
 
