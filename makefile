c1c095dc75b013c36931ffc7229b8e6be6007910
snapsid <siddharthchopra555@gmail.com>
2020-11-23 12:28:46 +0530

    creating makemove.c

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc vice.c init.c bitboard.c hashkeys.c c_board.c data.c attack.c io.c movegeneration.c -o vice
+	gcc vice.c init.c bitboard.c hashkeys.c c_board.c data.c attack.c io.c movegeneration.c makingmove.c -o vice
---
+++ makingmove.c
@@ -0,0 +1,22 @@
+#define HASH_PCE(pce,sq) (pos->posKey ^= (PieceKeys[(pce)][(sq)]))
+#define HASH_CA (pos->posKey ^= (CastleKeys[(pos->castlePerm)]))
+#define HASH_SIDE (pos->posKey ^= (SideKey))
+#define HASH_EP (pos->posKey ^= (PieceKeys[EMPTY][(pos->enPas)]))
+
+const int CastlePerm[120] = {
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 13, 15, 15, 15, 12, 15, 15, 14, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15,  7, 15, 15, 15,  3, 15, 15, 11, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15
+};
+// except 6 position all are set to 15
+// 15 bcoz casteling permission require 4 CountBit
+// 1111 = 15 
